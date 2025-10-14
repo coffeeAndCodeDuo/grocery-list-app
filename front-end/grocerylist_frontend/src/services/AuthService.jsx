e1ffceeba53b31ababf0001ed31e7b93b8256fd8
tiagocastilho <tiago.vaz.castilho@gmail.com>
2025-10-14 17:37:57 +0200

    changes on port and register URL for render test

--- front-end/grocerylist_frontend/src/services/AuthService.jsx
+++ front-end/grocerylist_frontend/src/services/AuthService.jsx
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:8081/grocery-list/api/auth';
+const BASE_URL = 'https://grocery-list-app-ek42.onrender.com/grocery-list/api/auth';
 
 export async function userRegister(firstName, lastName, email, password){
     const response = await fetch (`${BASE_URL}/register`, {
