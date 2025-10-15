b054b46d836bc14b96d2a971b2f45ba3b00abbaf
marianacmartins <marianacorreiam97@gmail.com>
2025-10-15 11:51:46 +0100

    changes in BASE_URL

--- front-end/grocerylist_frontend/src/services/AuthService.jsx
+++ front-end/grocerylist_frontend/src/services/AuthService.jsx
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://grocery-list-app-ek42.onrender.com/grocery-list/api/auth';
+const BASE_URL = 'https://grocery-list-production-d14a.up.railway.app/grocery-list/api/auth';
 
 export async function userRegister(firstName, lastName, email, password){
     const response = await fetch (`${BASE_URL}/register`, {
