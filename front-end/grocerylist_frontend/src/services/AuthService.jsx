e48251f6a00951f53e0734eceae4fcefc515c377
marianacmartins <marianacorreiam97@gmail.com>
2025-10-15 12:26:59 +0100

    back to original settings

--- front-end/grocerylist_frontend/src/services/AuthService.jsx
+++ front-end/grocerylist_frontend/src/services/AuthService.jsx
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://grocery-list-app-production-3323.up.railway.app/grocery-list/api/auth';
+const BASE_URL = 'http://localhost:8081/grocery-list/api/auth';
 
 export async function userRegister(firstName, lastName, email, password){
     const response = await fetch (`${BASE_URL}/register`, {
