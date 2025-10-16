beadd8687b279abb5bc2c996c4b5f8fb805087ba
tiagocastilho <tiago.vaz.castilho@gmail.com>
2025-10-16 11:49:16 +0200

    change port on spring config

--- back-end/src/main/java/grocery_list_app/config/SecurityConfig.java
+++ back-end/src/main/java/grocery_list_app/config/SecurityConfig.java
@@ -60,7 +60,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.addAllowedOrigin("http://localhost:5173");
+        configuration.addAllowedOrigin("http://localhost:5174");
         configuration.addAllowedHeader("*");
         configuration.addAllowedMethod("*");
         configuration.setAllowCredentials(true);
